33861b0d0d496cef87410156ee98ff8ecc399cea
Squidtoon99 <squidtoon99@gmail.com>
2022-10-15 01:43:51 -0500

    day 1

---
+++ lists/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod first;
+pub mod second;
+pub mod third;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        println!("I like seeing one test case pass");
+        assert!(true);
+    }
+}
